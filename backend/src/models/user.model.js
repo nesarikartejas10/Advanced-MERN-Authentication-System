8c9da21eb9fdc7bd268caf497ae11550a82d3d2b
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-16 10:03:25 +0530

    create user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      defaut: false,
+    },
+    isLoggedIn: {
+      type: Boolean,
+      default: false,
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    otp: {
+      type: String,
+      default: null,
+    },
+    otpExpiry: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
